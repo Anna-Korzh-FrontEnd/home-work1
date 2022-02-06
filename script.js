4cf7311827624ea9b8e723f53c7cb06ed42071ca
Anna-Korzh-FrontEnd <korzh_89@ukr.net>
2022-02-06 16:51:49 +0200

    first work

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+let name = prompt("Ваше имя?");
+alert("Hello, " + name);
+
+let a = 8;
+let b = 2;
+
+alert(a+b);
+
+alert(a-b);
+
+alert(a*b);
+
+alert(a/b);
+
+alert(a**b);
+
+console.log("New feature");
